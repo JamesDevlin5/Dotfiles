c52864078e3e4fd0dcd80be7ddddbdc4007ae514
James Devlin <devlin.ja@northeastern.edu>
2021-05-14 13:44:27 -0400

    Switched from old grep to ripgrep

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 
 # Global aliases
-alias -g G='| grep'
+alias -g G='| rg'
 alias -g P='| $PAGER'
 alias -g E='| $EDITOR'
 alias -g TF='| tail -f'
 alias -g DN='/dev/null'
-
+alias -g _='/dev/null'
