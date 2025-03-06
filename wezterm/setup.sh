15d3273a0e2d7b22def907fdd21ed6d90b69c72f
James Devlin <jamesdev3@gmail.com>
2025-03-05 22:29:17 -0600

    chore: Added Wezterm

---
+++ wezterm/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+if [ $# -eq 1 ] && [ "$1" = "clean" ]; then
+    rm ~/.config/wezterm/wezterm.lua ~/.config/wezterm/stylua.toml
+    return
+fi
+
+mkdir -p ~/.config/wezterm
+
+ln -s ~/.dotfiles/wezterm/wezterm.lua ~/.config/wezterm/wezterm.lua
+ln -s ~/.dotfiles/wezterm/stylua.toml ~/.config/wezterm/stylua.toml
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,69 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.audible_bell = 'Disabled'
+
+-- ms blink rate, set to 0 to disable blinking
+config.cursor_blink_rate = 800
+
+config.visual_bell = {
+  fade_in_function = 'EaseIn',
+  fade_in_duration_ms = 150,
+  fade_out_function = 'EaseOut',
+  fade_out_duration_ms = 150,
+}
+
+config.colors = {
+  visual_bell = '#202020',
+}
+
+config.default_cursor_style = 'BlinkingBar'
+
+--config.font = 'FiraCode Nerd Font'
+config.font_size = 12.0
+
+-- For example, changing the color scheme:
+config.color_scheme = 'OneHalfDark'
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.pane_focus_follows_mouse = true
+
+-- The filled in variant of the < symbol
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+
+-- The filled in variant of the > symbol
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+
+--[[
+config.tab_bar_style = {
+  active_tab_left = wezterm.format {
+    { Background = { Color = '#0b0022' } },
+    { Foreground = { Color = '#2b2042' } },
+    { Text = SOLID_LEFT_ARROW },
+  },
+  active_tab_right = wezterm.format {
+    { Background = { Color = '#0b0022' } },
+    { Foreground = { Color = '#2b2042' } },
+    { Text = SOLID_RIGHT_ARROW },
+  },
+  inactive_tab_left = wezterm.format {
+    { Background = { Color = '#0b0022' } },
+    { Foreground = { Color = '#1b1032' } },
+    { Text = SOLID_LEFT_ARROW },
+  },
+  inactive_tab_right = wezterm.format {
+    { Background = { Color = '#0b0022' } },
+    { Foreground = { Color = '#1b1032' } },
+    { Text = SOLID_RIGHT_ARROW },
+  },
+}
+--]]
+
+config.term = 'wezterm'
+
+-- and finally, return the configuration to wezterm
+return config
