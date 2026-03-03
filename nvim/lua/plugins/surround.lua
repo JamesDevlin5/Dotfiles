61c230c22aa80f8e7db9135c1fc61899f04ef1b0
James Devlin <jamesdevlin@gatech.edu>
2026-03-03 17:45:00 -0600

    Updates

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -2,14 +2,14 @@ return {
 	"kylechui/nvim-surround",
 	version = "*", -- Use for stability; omit to use `main` branch for the latest features
 	event = "VeryLazy",
-	opts = {
-		keymaps = {
-			normal = "s",
-			normal_cur = "ss",
-			normal_line = "S",
-			normal_cur_line = "SS",
-			visual = "s",
-			visual_line = "S",
-		},
-	},
+	config = function()
+		require("nvim-surround").setup()
+
+		vim.keymap.set("n", "s", "<Plug>(nvim-surround-normal)", { desc = "Add surrounding (normal)" })
+		vim.keymap.set("n", "ss", "<Plug>(nvim-surround-normal-cur)", { desc = "Add surrounding (current line)" })
+		vim.keymap.set("n", "S", "<Plug>(nvim-surround-normal-line)", { desc = "Add surrounding (line)" })
+		vim.keymap.set("n", "SS", "<Plug>(nvim-surround-normal-cur-line)", { desc = "Add surrounding (whole line)" })
+		vim.keymap.set("x", "s", "<Plug>(nvim-surround-visual)", { desc = "Add surrounding (visual)" })
+		vim.keymap.set("x", "S", "<Plug>(nvim-surround-visual-line)", { desc = "Add surrounding (visual line)" })
+	end,
 }
